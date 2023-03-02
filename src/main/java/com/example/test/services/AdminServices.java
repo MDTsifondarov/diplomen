740acd3c1afe0b45792ec0a0ad574cd8e4099539
MDTsifondarov18 <MDTsifondarov18@codingburgas.bg>
2023-03-02 10:53:12 +0200

    test commit

--- src/main/java/com/example/test/services/AdminServices.java
+++ src/main/java/com/example/test/services/AdminServices.java
@@ -1,10 +1,34 @@
 package com.example.test.services;
 
-//@Service
-//public class AdminServices {
-//     public void addBook(Books book) {
-//         String sql = "INSERT INTO books (title, author, isbn, year_published) VALUES (?, ?, ?, ?)";
-//         StatelessSession jdbcTemplate;
-//         jdbcTemplate.update(sql, book.getTitle(), book.getAuthor(), book.getIsbn(), book.getYearPublished());
-//     }
-// }
+import com.example.test.models.Book;
+import com.example.test.repositories.BooksRepository;
+import com.example.test.repositories.CustomersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AdminServices {
+    @Autowired
+    private BooksRepository bookRepository;
+
+    @Autowired
+    private CustomersRepository userRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(String id) {
+        Optional<Book> book = bookRepository.findById(id);
+        if(book.isPresent()) {
+            return book.get();
+        }
+//        else {
+//            throw new ResourceNotFoundException("Book not found with id " + id);
+//        }
+        return null;
+    }
+}
